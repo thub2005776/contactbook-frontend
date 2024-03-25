d295d483676b4dc08f3998cc888097298a0d933c
thub2005776 <thub2005776@student.ctu.edu.vn>
2024-03-25 22:56:52 +0700

    Tao trang them lien he

--- src/views/ContactAdd.vue
+++ src/views/ContactAdd.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="page">
-        <h4>Thêm Liên hệ</h4>
-        <ContactForm :contact="contact" @submit:contact="addContact"/>
+        <h4>Hiệu chỉnh Liên hệ</h4>
+        <ContactForm :contact="contact" @submit:contact="addContact" />
         <p>{{ message }}</p>
     </div>
 </template>
 <script>
-import ContactForm from "@/components/ContactFormAdd.vue";
+import ContactForm from "@/components/ContactForm.vue";
 import ContactService from "@/services/contact.service";
 export default {
     components: {
@@ -27,6 +27,10 @@ export default {
                 console.log(error);
             }
         },
+    },
+    created() {
+        this.contact = {};
+        this.message = "";
     },
 };
 </script>
